ad8c173f2ab3b544cf1b935bd453137c62832903
JRandle23 <jenny.randle23@gmail.com>
2021-03-30 14:26:39 -0700

    adding circle markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,6 +8,35 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 });
 
+// function createFeature(earthquakeData)  {
+
+
+//   earthquakeMarkers = earthquakeData.map((feature) =>
+//     L.circleMarker([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
+//       radius: magSize(feature.properties.mag),
+//       stroke: true, 
+//       color: "black", 
+//       opacity: 1,
+//       weight: 0.5, 
+//       fill: true,
+//       fillColor: magColor(feature.geometry.coordinates[2]),
+//       fillOpacity: 0.9
+//     })
+//     ((feature, layer) => {
+//       layer.bindPopUp("<h3>Magnitude: " + feature.properties.mag +
+//         "</h3><h3>Location: " + feature.properties.place +
+//         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+//     })
+//   )
+  
+
+
+//   var earthquakes = L.geoJSON(earthquakeMarkers);
+
+//   var mags = earthquakeData.map((d) => magSize(+d.properties.mag));
+
+  
+
 function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
@@ -20,8 +49,27 @@ function createFeatures(earthquakeData) {
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
+  
+  // function create(earthquakeMarker) {
+  //   markers = earthquakeMarker
+  // }
+  var geojsonMarkerOptions = {
+    radius: 8, 
+    fillColor: "orange", 
+    color: "yellow", 
+    weight: 1, 
+    opacity: 1, 
+    fillOpacity: 0.8
+  };
+
+  
+
+
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -31,10 +79,19 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Define satelite layer
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution:  "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 500,
+    maxZoom: 2,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  });
+  
+  
   var satelitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 250,
-    maxZoom: 18,
+    tileSize: 500,
+    maxZoom: 2,
     zoomOffset: -1,
     id: "mapbox/satellite-v9",
     accessToken: API_KEY
@@ -42,7 +99,8 @@ function createMap(earthquakes) {
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
+    tileSize: 500,
+    maxZoom: 2,
     id: "dark-v10",
     accessToken: API_KEY
   });
@@ -50,7 +108,8 @@ function createMap(earthquakes) {
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Satelite Map": satelitemap,
-    "Dark Map": darkmap
+    "Dark Map": darkmap,
+    "Light Map": lightmap
   };
 
   // Create overlay object to hold our overlay layer
@@ -66,6 +125,26 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [satelitemap, earthquakes]
   });
+
+//   function magColor(mag) {
+//     var color = "";
+//     if (mag <= 2) {color = "#ffffb2"; }
+//     else if (mag <= 3) {color = "#fecc5c"; }
+//     else if (mag <= 4) {color = "#fd8d3c"; }
+//     else if (mag <= 5) {color = "#f03b20"; }
+//     else {color = "#bd0026"; }
+  
+//   return color;
+//   };
+
+
+//   function magSize(mag){
+//     if (mag <= 1){
+//       return 8
+//     }
+//     return mag * 8;
+//   }
+// }
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
